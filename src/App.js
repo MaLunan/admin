6a2e122b3d4c7628161b83c80ac533f41bb7b718
ziyuemei <48012749+ziyuemei@users.noreply.github.com>
2020-01-06 11:20:38 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React,{Component} from 'react';
+import Home from './pages/Home'
 class App extends Component{
   render(){
     return(
-      <div>a</div>
+      <div>
+        <Home></Home>
+      </div>
     )
   }
 }
